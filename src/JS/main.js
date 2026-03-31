d4c9c0deb39906928f77eca8180a4965f755da1c
Shadowbolts <vladpilipenko05@gmail.com>
2026-03-31 18:29:59 +0300

    .

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,4 +1,4 @@
-// Selectors
+// Selectors 
 const toDoInput = document.querySelector(".todo-input");
 const toDoBtn = document.querySelector(".todo-btn");
 const toDoList = document.querySelector(".todo-list");
